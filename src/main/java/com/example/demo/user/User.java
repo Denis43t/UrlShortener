78b9fc8e89e71c358d1c8c5c7af25cd904916f3c
ikg100 <ihorkolybaba@gmail.com>
2025-01-12 19:09:05 +0200

    change username length

--- src/main/java/com/example/demo/user/User.java
+++ src/main/java/com/example/demo/user/User.java
@@ -18,7 +18,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 100, nullable = false, unique = true)
+    @Column(length = 50, nullable = false, unique = true)
     private String username;
 
     @Column(length = 100, nullable = false)
