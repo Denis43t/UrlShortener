f30d19982827d29323e463e33f6542b80caac908
ikg100 <ihorkolybaba@gmail.com>
2025-01-12 18:53:01 +0200

    add constants to UserValidator

--- src/main/java/com/example/demo/user/UserValidator.java
+++ src/main/java/com/example/demo/user/UserValidator.java
@@ -7,9 +7,11 @@ import java.util.Objects;
 
 @Service
 public class UserValidator {
+    private static final int MIN_USERNAME_LENGTH = 5;
+    private static final int MAX_USERNAME_LENGTH = 50;
 
     public boolean isValidUsernameFormat(String username) {
-        return username.length() >= 5 && username.length() <= 50;
+        return username.length() >= MIN_USERNAME_LENGTH && username.length() <= MAX_USERNAME_LENGTH;
     }
 
     public boolean isValidPasswordFormat(String password) {
