36823a4b17a5159919574ed0fc56d459f3fb1088
ikg100 <ihorkolybaba@gmail.com>
2025-01-12 21:00:55 +0200

    add MIN_PASSWORD_LENGTH

--- src/main/java/com/example/demo/user/UserValidator.java
+++ src/main/java/com/example/demo/user/UserValidator.java
@@ -9,13 +9,14 @@ import java.util.Objects;
 public class UserValidator {
     private static final int MIN_USERNAME_LENGTH = 5;
     private static final int MAX_USERNAME_LENGTH = 50;
+    private static final int MIN_PASSWORD_LENGTH = 8;
 
     public boolean isValidUsernameFormat(String username) {
         return username.length() >= MIN_USERNAME_LENGTH && username.length() <= MAX_USERNAME_LENGTH;
     }
 
     public boolean isValidPasswordFormat(String password) {
-        if (Objects.isNull(password) || password.length() < 8) {
+        if (Objects.isNull(password) || password.length() < MIN_PASSWORD_LENGTH) {
             return false;
         }
 
