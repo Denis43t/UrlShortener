00825af0e16bdaceea623e0d688d1aa4cb602ccf
ikg100 <ihorkolybaba@gmail.com>
2025-01-15 14:50:50 +0200

    delete profile from PostgresTestConfig

--- src/test/java/com/example/demo/PostgresTestConfig.java
+++ src/test/java/com/example/demo/PostgresTestConfig.java
@@ -4,7 +4,6 @@ package com.example.demo;
 import io.zonky.test.db.postgres.embedded.EmbeddedPostgres;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 /**
  * Configuration class for setting up an embedded PostgreSQL database for testing.
@@ -14,20 +13,13 @@ import org.springframework.context.annotation.Profile;
  * the database before running any tests and provides logging in case of any
  * failures during the setup.
  */
-@Profile("dev")
 @Configuration
 @Slf4j
 public class PostgresTestConfig {
 
-    /**
-     * Static field represents an embedded instance of a PostgreSQL database
-     * used specifically for testing purposes.
-     */
-    private static EmbeddedPostgres postgres;
-
     static {
         try {
-            postgres = EmbeddedPostgres.builder()
+            EmbeddedPostgres.builder()
                     .setPort(15432)
                     .start();
         } catch (Exception e) {
