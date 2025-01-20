c2b8a1068bfb9672e1c04a1f836d9e197dae8496
ikg100 <ihorkolybaba@gmail.com>
2025-01-20 15:20:07 +0200

    add expiration to statistics and improved business logic

--- src/main/java/com/goit/url_shortener/statistics/StatsUrlDto.java
+++ src/main/java/com/goit/url_shortener/statistics/StatsUrlDto.java
@@ -1,6 +1,7 @@
 package com.goit.url_shortener.statistics;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,10 +9,12 @@ import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class StatsUrlDto {
     private String shortUrl;
     private String longUrl;
     private long visits;
     private boolean isActive;
     private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
 }
