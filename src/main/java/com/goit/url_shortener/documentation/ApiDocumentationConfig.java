9a2e5c27e94a36916a9569c1d36f15347ab4cd78
Denis Bunak <xisi926@ukr.net>
2025-01-20 15:51:35 +0200

    Refactor method getApiDoc

--- src/main/java/com/goit/url_shortener/documentation/ApiDocumentationConfig.java
+++ src/main/java/com/goit/url_shortener/documentation/ApiDocumentationConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo.documentation;
+package com.goit.url_shortener.documentation;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -9,12 +9,12 @@ public class ApiDocumentationConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/openapiV1.json")
+        registry.addResourceHandler("/api/v1/docs/openapiV1.json")
                 .addResourceLocations("classpath:/static/documentation/openapiV1.json")
                 .setCachePeriod(3600)
                 .resourceChain(true);
 
-        registry.addResourceHandler("/openapiV2.json")
+        registry.addResourceHandler("/api/v2/docs/openapiV2.json")
                 .addResourceLocations("classpath:/static/documentation/openapiV2.json")
                 .setCachePeriod(3600)
                 .resourceChain(true);
--- src/main/java/com/goit/url_shortener/documentation/ApiDocumentationController.java
+++ src/main/java/com/goit/url_shortener/documentation/ApiDocumentationController.java
@@ -1,16 +1,16 @@
-package com.example.demo.documentation;
+package com.goit.url_shortener.documentation;
 
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * In this application is using documentation from yaml file for supporting the principle of cline code.
  *  That's why for getting api documentation is used Api documentation controller.
  */
-@Controller
-@RequestMapping("**")
+@RestController
+@RequestMapping("/api")
 public class ApiDocumentationController {
 
     /**
@@ -18,7 +18,7 @@ public class ApiDocumentationController {
      *
      * @return a redirect URL pointing to the stable OpenAPI documentation (openapiV1.json).
      */
-    @GetMapping("/api/docs")
+    @GetMapping("/docs")
     public String getApiStableDocumentation() {
         return "redirect:/documentation/openapiV1.json";
     }
@@ -28,7 +28,7 @@ public class ApiDocumentationController {
      *
      * @return a redirect URL pointing to OpenAPI documentation for version 1 (openapiV1.json).
      */
-    @GetMapping({"/api/v1/docs", "/openapiV1.json"})
+    @GetMapping({"/v1/docs", "/v1/docs/openapiV1.json"})
     public String getApiV1Documentation() {
         return "redirect:/documentation/openapiV1.json";
     }
@@ -38,7 +38,7 @@ public class ApiDocumentationController {
      *
      * @return a redirect URL pointing to OpenAPI documentation for version 2 (openapiV2.json).
      */
-    @GetMapping({"/api/v2/docs", "/openapiV2.json"})
+    @GetMapping({"/v2/docs", "/v1/docs/openapiV2.json"})
     public String getApiV2Documentation() {
         return "redirect:/documentation/openapiV2.json";
     }
